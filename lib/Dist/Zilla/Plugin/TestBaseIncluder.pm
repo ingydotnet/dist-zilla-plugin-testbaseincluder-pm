c1ba74194cb05c8f801a857280fc94081c5e4eca
Ingy döt Net <ingy@ingy.net>
2014-06-14 01:15:55 -0700

    0.02

--- lib/Dist/Zilla/Plugin/TestBaseIncluder.pm
+++ lib/Dist/Zilla/Plugin/TestBaseIncluder.pm
@@ -33,14 +33,16 @@ has blacklist => (
 
 sub gather_files {
   my $self = shift;
-  my $testbase = '../test-base-pm';
-  if (
-    -d "$testbase/.git"
-  ) {
-    eval "use lib '$testbase/lib'; 1" or die $@;
-    $self->SUPER::gather_files(@_);
-    return;
-  }
+  for my $prefix (qw(.. ../..)) {
+    my $testbase = "$prefix/test-base-pm";
+    if (
+      -d "$testbase/.git"
+    ) {
+      eval "use lib '$testbase/lib'; 1" or die $@;
+      $self->SUPER::gather_files(@_);
+      return;
+    }
+}
   die "Test::Base repo missing or not in right state";
 }
 
@@ -49,35 +51,3 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
-
-=encoding utf8
-
-=head1 NAME
-
-Dist::Zilla::Plugin::TestBaseIncluder - Ship your Test::Base version
-
-=head1 SYNOPSIS
-
-In dist.ini:
-
-   [TestBaseIncluder]
-
-=head1 DESCRIPTION
-
-This module includes the version of Test::Base on your system with your module
-dist.
-
-=head1 AUTHOR
-
-Ingy döt Net <ingy@cpan.org>
-
-=head1 COPYRIGHT
-
-Copyright (c) 2014. Ingy döt Net.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-See http://www.perl.com/perl/misc/Artistic.html
-
-=cut
---
+++ test/use_ok.t
@@ -0,0 +1,5 @@
+use Test::More;
+
+use_ok 'Dist::Zilla::Plugin::TestBaseIncluder';
+
+done_testing;
