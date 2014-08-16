972f065cfc414adad5fb64ffe295b816f472b21f
Ingy döt Net <ingy@ingy.net>
2014-08-15 21:18:11 -0700

    CPAN Release 0.07
    - Remove t/use_ok.t

--- lib/Dist/Zilla/Plugin/TestBaseIncluder.pm
+++ lib/Dist/Zilla/Plugin/TestBaseIncluder.pm
@@ -1,5 +1,5 @@
 package Dist::Zilla::Plugin::TestBaseIncluder;
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 use Moose;
 
--- test/use_ok.t
+++
@@ -1,5 +0,0 @@
-use Test::More;
-
-use_ok 'Dist::Zilla::Plugin::TestBaseIncluder';
-
-done_testing;
